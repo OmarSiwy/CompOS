d7aab9a947d1efb352e5c5f6d4ac953bbdb95c82
OmarSiwy <160556310+OmarSiwy@users.noreply.github.com>
2024-10-26 19:32:00 +0000

    Deploying to gh-pages from @ OmarSiwy/A-RTOS-M@11b74ff989030447e72efda9dd52804ac62ab574 🚀

---
+++ search/all_6.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['my_20priority_20right_20now_20is_20to_20fully_20finalize_20the_20rtos_20codebase_20then_20start_20on_20this_20later_20on_0',['My priority right now, is to fully finalize the RTOS codebase then start on this later on',['../md_inc_2_h_a_l_2_r_e_a_d_m_e.html#autotoc_md1',1,'']]]
+];
---
+++ search/pages_5.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['work_20in_20progress_0',['Still a heavy work in progress',['../md_inc_2_h_a_l_2_r_e_a_d_m_e.html',1,'']]]
+];
